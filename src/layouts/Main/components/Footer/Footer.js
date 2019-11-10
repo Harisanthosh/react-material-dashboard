627d4b010a44ed4355f96e013fdabe5c733c516a
Hari Santhosh <harisanthosh94@gmail.com>
2019-11-10 07:17:55 +0530

    Updated author info

--- src/layouts/Main/components/Footer/Footer.js
+++ src/layouts/Main/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = props => {
         &copy;{' '}
         <Link
           component="a"
-          href="https://devias.io/"
+          href="http://bludle.com"
           target="_blank"
         >
           Waste No More
